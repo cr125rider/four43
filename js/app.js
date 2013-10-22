61099fd5820a6c8b727a75e8efb24faccbfe49b5
Seth Miller <cr125rider@gmail.com>
2013-10-22 00:03:28 -0500

    ADD - Tumblr content, fix CSS, modified color pallet.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
-$(document).ready(function() {
-	$(window).on('scroll', function() {
-		var scrollTop = $('html').scrollTop() + $('body').scrollTop();
-		$('body').css('background-position', 'center ' + (-scrollTop / 1.5) + 'px');
+define(['marionette', 'backbone', 'handlebars', 'jquery', 'bootstrap'], function(Marionette, Backbone, Handlebars, $jq) {
+	$jq(document).ready(function() {
+		$jq(window).on('scroll', function() {
+			var scrollTop = $jq('html').scrollTop() + $jq('body').scrollTop();
+			$jq('body').css('background-position', 'center ' + (-scrollTop / 2) + 'px');
+		});
+		$jq(window).trigger('scroll');
 	});
+	
+	var App = new Marionette.Application();
+
+	//Backbone Overrides for Handlebars
+	Marionette.TemplateCache.prototype.compileTemplate = function(rawTemplate) {
+		return Handlebars.compile(rawTemplate);
+	};
+
+	App.addRegions({
+		mainRegion: "#tumblr-content"
+	});
+
+	App.on("initialize:after", function(options) {
+		if (Backbone.history) {
+			Backbone.history.start();
+		}
+	});
+
+	return App;
 });
