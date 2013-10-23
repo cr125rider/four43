d11a67fb2a4c7a04e8420fe42ef42b5ce9a2fcf0
Seth Miller <cr125rider@gmail.com>
2013-10-23 00:21:35 -0500

    FIX - Mobile offsets, disabled mobile scrolling and added four43 to the brand.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 define(['marionette', 'backbone', 'handlebars', 'jquery', 'bootstrap'], function(Marionette, Backbone, Handlebars, $jq) {
 	$jq(document).ready(function() {
-		$jq(window).on('scroll', function() {
-			var scrollTop = $jq('html').scrollTop() + $jq('body').scrollTop();
-			$jq('body').css('background-position', 'center ' + (-scrollTop / 2) + 'px');
-		});
-		$jq(window).trigger('scroll');
+		if ($jq(window).width > 750) {
+			$jq(window).on('scroll', function() {
+				var scrollTop = $jq('html').scrollTop() + $jq('body').scrollTop();
+				$jq('body').css('background-position', 'center ' + (-scrollTop / 2) + 'px');
+			});
+			$jq(window).trigger('scroll');
+		}
+		else {
+			$jq(window).on('scroll', function() {
+				var scrollTop = $jq('html').scrollTop() + $jq('body').scrollTop();
+				if(scrollTop > 35) {
+					$jq('.navbar-brand').show();
+				}
+				else {
+					$jq('.navbar-brand').hide();
+				}
+			});
+			$jq(window).trigger('scroll');
+		}
 	});
-	
+
 	var App = new Marionette.Application();
 
 	//Backbone Overrides for Handlebars
