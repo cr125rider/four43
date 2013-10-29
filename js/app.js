6d05ccf04bec6874187134772fa9d72d19704b19
Seth Miller <cr125rider@gmail.com>
2013-10-28 23:00:08 -0500

    FIX - Grammar, added social icons and icon font.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,21 @@
 define(['marionette', 'backbone', 'handlebars', 'jquery', 'bootstrap'], function(Marionette, Backbone, Handlebars, $jq) {
 	$jq(document).ready(function() {
-		if ($jq(window).width > 750) {
-			$jq(window).on('scroll', function() {
+		$jq(window).on('scroll', function() {
+			if ($jq(window).width() > 750) {
 				var scrollTop = $jq('html').scrollTop() + $jq('body').scrollTop();
 				$jq('body').css('background-position', 'center ' + (-scrollTop / 2) + 'px');
-			});
-			$jq(window).trigger('scroll');
-		}
-		else {
-			$jq(window).on('scroll', function() {
+			}
+			else {
 				var scrollTop = $jq('html').scrollTop() + $jq('body').scrollTop();
-				if(scrollTop > 35) {
+				if (scrollTop > 35) {
 					$jq('.navbar-brand').show();
 				}
 				else {
 					$jq('.navbar-brand').hide();
 				}
-			});
-			$jq(window).trigger('scroll');
-		}
+			}
+		});
+		$jq(window).trigger('scroll');
 	});
 
 	var App = new Marionette.Application();
