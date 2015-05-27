d277003177bf29d39632158eb8d8d2cb130b995e
Seth Miller <seth@four43.com>
2015-05-27 18:07:32 -0500

    Adding components, restructuring. Much better styling.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,47 +24,21 @@ class About extends React.Component {
 
     render() {
         return (
-            <section id="about">
-                <div className="section-header row">
-                    <h2 className="col-sm-12">About Me</h2>
-                    <p className="col-sm-12">
-                        I am a {this.state.age} year old software developer based out of Minneapolis, Minnesota. I am passionate about the open web and the opportunities it brings to communication and business around the
-                        world. Traditional database and other storage systems are also one of my interests. Combining
-                        the two with security in the center, I help create products that solve a wide range of
-                        problems.
-                    </p>
-                </div>
-                <div id="my-tech" className="row">
-                    <div className="col-sm-4 tech-highlight">
-                        <div className="tech-container">
-                            <h3>Web</h3>
-                            <p>
-                                Experience in connecting users with content they need. I have helped
-                                create web apps and have designed and created web APIs for consumption by other
-                                systems and mobile devices. An API centric model is essential for a growing
-                                flexible business.
-                            </p>
-                        </div>
+            <section id="about" className="container-fluid">
+                <div className="row">
+                    <div className="section-header col-sm-4 col-sm-offset-3 section-header">
+
+                        <h2>About</h2>
+                        <h3>Data, Security, and the Web</h3>
+                        <p>
+                            Seth is a software developer based out of Minneapolis, Minnesota. He is passionate about
+                            the open web and the opportunities it brings to communication and businesses around the
+                            world. Using big data processing, combined with smart analytics, and responsive user interfaces,
+                            Seth creates real-time applications that solve problems for modern day business needs.
+                        </p>
                     </div>
-                    <div className="col-sm-4 tech-highlight">
-                        <div className="tech-container">
-                            <h3>Big Data</h3>
-                            <p>
-                                With a background in traditional relational databases and experience in the more
-                                loose No-SQL data stores, I have worked with a broad range of storage systems for
-                                a wide range data set requirements.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col-sm-4 tech-highlight">
-                        <div className="tech-container">
-                            <h3>Security</h3>
-                            <p>
-                                Everyone has the right to share content securely and anonymously. Systems I help create
-                                are secure using the latest proven web and cryptographic technologies to keep company
-                                and user data safe.
-                            </p>
-                        </div>
+                    <div id="headshot-container" className="col-sm-2">
+                        <img id="headshot" src="./img/headshot.jpg" alt="Seth's pretty mug" className="img-circle" />
                     </div>
                 </div>
             </section>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export default
+class Nav extends React.Component {
+    render() {
+        return (
+            <div>
+                <nav id="navbar-top" className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">four43</a>
+                    </div>
+
+                    <div className="collapse navbar-collapse navbar-ex1-collapse">
+                        <ul className="nav navbar-nav">
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#work">Work</a></li>
+                            <li><a href="#projects">Projects</a></li>
+                            <li><a href="#values">Values</a></li>
+                        </ul>
+                    </div>
+                </nav>
+                <div id="nav-row" className="row">
+                    <nav id="main-nav" className="col-sm-6 col-sm-offset-3 block-container" role="navigation">
+                        <ul className="nav nav-pills">
+                            <li>
+                                <a href="#about">About</a>
+                            </li>
+                            <li>
+                                <a href="#work">Work</a>
+                            </li>
+                            <li>
+                                <a href="#projects">Projects</a>
+                            </li>
+                            <li>
+                                <a href="#values">Values</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        );
+    }
+}
