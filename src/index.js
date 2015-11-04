9d5ac69d2de3953efbfb7be19632b20ba48dfad8
Seth Miller <seth@four43.com>
2015-11-03 20:43:03 -0600

    Cleanup, added console text.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ import React from 'react';
 import App from './App';
 
 React.render(<App />, document.getElementById('root'));
+
+console.log("\ \
+ __                 _  _  ____  \n\
+/  _|               | || ||___ \\\n\
+| |_ ___  _   _ _ __| || |_ __) |\n\
+|  _/ _ \\| | | | '__|__   _|__ < \n\
+| || (_) | |_| | |     | | ___) |\n\
+|_| \\___/ \\__,_|_|     |_||____/\n\
+The professional home of Seth Miller. Please take a look around.\n\
+\n\
+This website was created using React and a React Hotloader. It uses great technology created by some really smart people.\n\
+The full source is available at: https://github.com/four43/four43/tree/gh-pages\n\
+\n\
+Like what you see? Contact me: seth@four43.com\
+");
